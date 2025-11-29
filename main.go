525dc787253ca08b7c1767a2500f289de6510eea
Mikhail Yakovlev <mikhailyakov1ev@mail.ru>
2025-11-29 17:39:51 +0500

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ func main() {
     http.HandleFunc("/", helloHandler)
     
     fmt.Println("Starting server on :8080...")
-    fmt.Println("Visit: http://localhost:8080")
     
     err := http.ListenAndServe(":8080", nil)
     if err != nil {
