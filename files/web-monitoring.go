a01f8fffd95f26cb1f5189f6b963d16cef21234a
OSHPA-6-S-43 <oshpa-6-s-43@innoca.local>
2025-12-01 00:49:34 +0500

    start

---
+++ files/web-monitoring.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", helloHandler)
+
+    fmt.Println("Starting server on :8080...")
+    fmt.Println("Visit: http://localhost:8080")
+
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        fmt.Printf("Error starting server: %s\n", err)
+    }
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello World!")
+}
